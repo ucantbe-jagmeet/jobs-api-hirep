4aed0d73eec2bbfa5359a74f648bbb8d69ec7829
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-05-14 18:14:49 +0530

    xss-clean package

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ require("express-async-errors");
 
 const helmet = require("helmet");
 const cors = require("cors");
-const xss = require("xss-clear");
+const xss = require("xss-clean");
 const rateLimiter = require("express-rate-limit");
 
 const express = require("express");
