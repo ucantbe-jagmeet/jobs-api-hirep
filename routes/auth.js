c379b791a7eb15c80553f99c835f01a99b31ef62
This-Is-Prince <jagmeetsingh2921@gmail.com>
2024-08-23 18:01:11 +0530

    some changes in update user api added

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const { login, register } = require("../controllers/auth");
+const { login, register, updateUser } = require("../controllers/auth");
 
 router.post("/register", register);
 router.post("/login", login);
