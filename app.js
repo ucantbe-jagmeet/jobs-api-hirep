daf76c8f59c46b9e055648538a79c967e47951d4
Jagmeet Singh <89521118+ucantbe-jagmeet@users.noreply.github.com>
2024-08-23 16:22:06 +0530

    Update app.js

--- app.js
+++ app.js
@@ -39,7 +39,24 @@ app.use(
 app.use(express.json());
 
 app.use(helmet());
-app.use(cors());
+const allowedOrigins = [
+  'http://localhost:3000',
+  'https://hi-rep-developers.vercel.app'
+];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  methods: 'GET,POST,PUT,DELETE,OPTIONS',
+  allowedHeaders: 'Content-Type,Authorization'
+};
+
+app.use(cors(corsOptions));
 app.use(xss());
 
 // routes
